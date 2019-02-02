a97bbd6a16fe5c62eb99da89d6a664e28dc76873
maezono <maezonog@gmail.com>
2019-02-02 21:35:30 +0900

    課題、発展課題
    課題、発展課題終了

--- Assets/StartController.cs
+++ Assets/StartController.cs
@@ -17,14 +17,12 @@ public class StartController : MonoBehaviour {
 		this.difference = unitychan.transform.position.z - this.transform.position.z;
 		this.myRigidbody = GetComponent<Rigidbody> ();
 
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		this.transform.position = new Vector3 (0, this.transform.position.y, this.unitychan.transform.position.z- difference);
-
 
 
 	}
